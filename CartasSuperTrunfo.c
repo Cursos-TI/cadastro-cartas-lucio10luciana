c643c73bc7f3f6cbb7594d00aca8c92e9beaf337
lucio10luciana <ivai.lucio@hotmail.com>
2026-06-26 22:16:56 +0000

    criaçãodas cartas fase novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,32 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char  carta1   [10];
+char  estado1   [10];
+char  cidade1  [10];
+char   codigo1;
+int   população1;
+float pib1;
+float area1;
+int   pontosturisticos1; 
+float densidadepop1;
+float pibpercapita1;
+
+// segunda carta
+
+char  carta2   [10];
+char  estado2   [10];
+char  cidade2  [10];
+char   codigo2;
+int   população2;
+float pib2;
+float area2;
+int   pontosturisticos2; 
+float densidadepop2;
+float pibpercapita2;
+
+
+
 
   // Área para entrada de dados
 
